62ebf509a5b324cdd1032bf85dca767e45854c94
jallend1 <jallend1@gmail.com>
2020-03-20 12:52:27 -0700

    Adds day eighteen reduce method challenge

---
+++ 18/app.js
@@ -0,0 +1,15 @@
+const timeNodes = [...document.querySelectorAll('[data-time]')];
+const seconds = timeNodes
+    .map(node => node.dataset.time) //Array of just the times
+    .map(timeCode => {
+        const [mins, secs] = timeCode.split(':').map(parseFloat);   // Separates the minutes and seconds into independent variables
+        return (mins * 60) + secs;
+    })
+    .reduce((total, seconds) => total + seconds);
+
+let secondsLeft = seconds;
+const hours = Math.floor(secondsLeft / 3600);
+secondsLeft = secondsLeft % 3600;
+const mins = Math.floor(secondsLeft / 60);
+secondsLeft = secondsLeft % 60;
+console.log(hours, mins, secondsLeft);
