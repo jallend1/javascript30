34db47b374dde4d5704366691f7aa2bac5cba363
jallend1 <jallend1@gmail.com>
2020-03-27 17:32:23 -0700

    Adds whack-a-mole exercise

--- 29/app.js
+++ 29/app.js
@@ -3,6 +3,26 @@ const endTime = document.querySelector('.display__end-time');
 const buttons = document.querySelectorAll('[data-time]');
 let countdown;
 
+function displayEndTime(timestamp){
+    const end = new Date(timestamp);
+    const hour = end.getHours();
+    const minutes = end.getMinutes();
+    endTime.textContent = `Be back at ${hour > 12 ? hour -12 : hour}:${ minutes < 10 ? '0' : ''}${minutes}`;
+}
+
+function displayTimeLeft(seconds){
+    const minutes = Math.floor(seconds / 60);
+    const remainderSeconds = seconds % 60;
+    const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+    timerDisplay.textContent = display;
+    document.title = display;
+}
+
+function startTimer(){
+    const seconds = parseInt(this.dataset.time);
+    timer(seconds);
+}
+
 function timer(seconds){
     const now = Date.now();
     const then = now + seconds * 1000;
@@ -17,27 +37,6 @@ function timer(seconds){
         }
         displayTimeLeft(secondsLeft);
     }, 1000);
-}
-
-function displayTimeLeft(seconds){
-    const minutes = Math.floor(seconds / 60);
-    const remainderSeconds = seconds % 60;
-    const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
-    timerDisplay.textContent = display;
-    document.title = display;
-}
-
-function displayEndTime(timestamp){
-    const end = new Date(timestamp);
-    const hour = end.getHours();
-    const minutes = end.getMinutes();
-    endTime.textContent = `Be back at ${hour > 12 ? hour -12 : hour}:${ minutes < 10 ? '0' : ''}${minutes}`;
-
-}
-
-function startTimer(){
-    const seconds = parseInt(this.dataset.time);
-    timer(seconds);
 }
 
 buttons.forEach(button => button.addEventListener('click', startTimer));
--- 30/app.js
+++ 30/app.js
@@ -1,3 +1,50 @@
 const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
+let lastHole;
+let score = 0;
+let timeUp = false;
+
+
+function bonk(e){
+    if(!e.isTrusted) return; // Phonies!
+    score++;
+    this.classList.remove('up');
+    scoreBoard.textContent = score;
+}
+
+moles.forEach(mole => mole.addEventListener('click', bonk));
+
+function peep() {
+    const time = randomTime(200, 1000);
+    const hole = randomHole(holes);
+    hole.classList.add('up');
+    setTimeout(() => {
+        hole.classList.remove('up');
+        if(!timeUp) peep();
+    }, time)
+    console.log(time, hole);
+}
+
+function randomHole(holes){
+    const idx = Math.floor(Math.random() * holes.length);
+    const hole = holes[idx];
+    if(hole === lastHole){
+        console.log('no');
+        return randomHole(holes);
+    }
+    lastHole = hole;
+    return hole;
+}
+
+function randomTime(min, max){
+    return Math.round(Math.random() * (max - min) + min);
+}
+
+function startGame(){
+    score = 0;
+    scoreBoard.textContent = 0;
+    timeUp = false;
+    peep();
+    setTimeout(() => timeUp = true, 10000);
+}
